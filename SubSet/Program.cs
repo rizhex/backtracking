b056d5cdd8a31c36eb0d16ca6cfee84d0cbddd59
rizhe.Orlando <rizhexDev@gmail.com>
2022-10-05 15:35:15 -0500

    upload

---
+++ SubSet/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] conj  = {1, 2, 4, 3, 32};
+bool[] mask = new bool[conj.Length];
+subset(conj, conj.Length - 1, mask);
+
+static void subset(int[] conj, int n, bool[] mask){
+    
+    if(n == -1){
+        Print(mask, conj);
+        return;
+    }
+
+    mask[n] = true;
+    subset(conj, n-1, mask);
+    mask[n] = false;
+    subset(conj, n-1, mask);
+}
+
+static void Print(bool[] mask, int[] conj){
+    for(int i = 0; i < mask.Length; i++){
+        if(mask[i]){
+            System.Console.Write(conj[i] + " ");
+        }
+    }
+    System.Console.WriteLine("");
+}
